74c8647238568ee01475a44f1530fcc11fe3dc5a
akrawczyk <agammkrawczyk@gmail.com>
2020-10-29 20:14:00 +0100

    testing homes location, title and img from mock data

--- src/app/components/homes/homes.component.spec.ts
+++ src/app/components/homes/homes.component.spec.ts
@@ -22,4 +22,17 @@ describe('HomesComponent', () => {
   it('should show homes', () => {
     expect(fixture.nativeElement.querySelectorAll('[data-test="home"]').length).toBe(3);
   });
+
+  it('should show homes info', () => {
+    expect(fixture.nativeElement.querySelector('[data-test="title"]').innerText).toEqual('home 1');
+  });
+  it('should show homes location', () => {
+    expect(fixture.nativeElement.querySelector('[data-test="location"]').innerText).toEqual('Boston');
+  });
+/*  it('should show homes location 2', () => {
+    expect(fixture.nativeElement.querySelector('[data-test="location"]').innerText).toEqual('Chicago');
+  });*/
+  it('should show homes image', () => {
+    expect(fixture.nativeElement.querySelector('[data-test="image"]')).toBeTruthy();
+  });
 });
--- src/app/components/homes/homes.component.ts
+++ src/app/components/homes/homes.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {of} from 'rxjs';
 
 @Component({
   selector: 'app-homes',
@@ -7,9 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomesComponent implements OnInit {
 
-  constructor() { }
+  homes$;
 
-  ngOnInit(): void {
+  constructor() {
+  }
+
+  ngOnInit() {
+    //  this.homes$ = this.dataService.getHomes$();
+    this.homes$ = of([
+      {
+        title: 'home 1',
+        image: 'assets/home.jpg',
+        location: 'Boston'
+      },
+      {
+        title: 'home 2',
+        image: 'assets/home2.jpg',
+        location: 'Chicago'
+      },
+      {
+        title: 'home 2',
+        image: 'assets/home3.jpg',
+        location: 'Alabama'
+      }
+    ]);
   }
 
 }
